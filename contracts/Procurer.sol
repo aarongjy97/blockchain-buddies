ced35b0a976869be8a7b071c0fb01d4beb4ab2be
Aaron Goh <aarongjy97@gmail.com>
2021-03-11 21:23:50 +0800

    Refactored Procurer.sol

--- contracts/Procurer.sol
+++ contracts/Procurer.sol
@@ -4,17 +4,9 @@ import './Supplier.sol';
 import './Courier.sol';
 
 contract Procurer {
-    address owner;
-    uint256 orderId;
 
-    constructor() public {
-        owner = msg.sender;
-    }
+    address _owner;
 
-    mapping(address => Employee) employees;
-    mapping(uint256 => PurchaseOrder) purchaseOrders;
-
-    // Struct
     struct PurchaseOrder {
         address employee;
         address supplier;
@@ -31,42 +23,62 @@ contract Procurer {
         string name;
         bool isFinance;
     }    
-    
-    modifier isFinanceEmployee(address employeeAddress) {
-        require(employees[employeeAddress].isFinance, 'Employee is not a finance employee');
+
+    mapping(address => Employee) employees;
+    mapping(uint256 => PurchaseOrder) internalPurchaseOrders;
+
+    constructor() public {
+        _owner = msg.sender;
+    }
+
+    /* ==================== Modifiers ==================== */
+
+    modifier isFinanceEmployee {
+        require(employees[msg.sender] != 0, "Only employee can perform this function");
+        require(employees[msg.sender].isFinance, 'Employee is not a finance employee');
         _;
     }
    
     modifier isLogisticsEmployee(address employeeAddress) {
-        require(!employees[employeeAddress].isFinance, 'Employee is not a logistics employee');
+        require(employees[msg.sender] != 0, "Only employee can perform this function");
+        require(!employees[msg.sender].isFinance, 'Employee is not a logistics employee');
         _;
     }
 
-    function createPurchaseOrder(Product product, uint256 quantity, uint256 price) public isLogisticsEmployee(msg.sender) returns (uint256) {
-        purchaseOrders[orderId] = PurchaseOrder(msg.sender, product.supplier, product.courier, product.productId, price, quantity, block.timestamp, employees[msg.sender].name, false);
-
-        // @TODO push purchase order to marketplace so supplier can see
-        return orderId++;
+    /**
+     * Creates a purchase order for the finance team to approve.
+     * Only Logistics Employees are allowed to create purchase orders.
+     */
+    function createPurchaseOrder(Product product, uint256 quantity, uint256 price) public isLogisticsEmployee returns (uint256) {
     }
     
-    function approvePurchaseOrder(uint256 orderId) public isFinanceEmployee(msg.sender) {
-        
+    /**
+     * Approves a purchase order internally, and the order will be pushed to the market
+     * for the supplier to approve.
+     * Changes the status of the purchase order from Ordered to Approved.
+     * Only Finance Employees are allowed to approve purchase orders.
+     * Purchase orders that are rejected previously cannot be approved.
+     * @calls Market contract
+     */
+    function approvePurchaseOrder(uint256 orderId) public isFinanceEmployee {
     }
     
-    function cancelPurchaseOrder(uint256 orderId) public {
-        purchaseOrders[orderId].isClosed = true;
+    /**
+     * Rejects a purchase order internally, and the order cannot be approved again.
+     * Changes the status of the purchase order from Ordered to Rejected.
+     * Only Finance Employees are allowed to approve purchase orders.
+     */
+    function rejectPurchaseOrder(uint256 orderId) public {
     }
     
-    function deliveredByDelivery(uint256 orderId, uint256 companyId) public {
-      require(companies[companyId].purchaseOrders[orderId].LogisticsEmployee == msg.sender);     // Check that the person accepting the order is the person who created the order
-      // acknowledge goods in good condition (Skipz)
-      
-      // wire money to supplier
-      // ERC token transferFrom...
-
-      // close purchase order
-      companies[companyId].purchaseOrders[orderId].isClosed = true;
-      emit orderDelivered(orderId);
+    /**
+     * Confirms the delivery has been received by the delivery courier.
+     * Changes the status of the purchase order from Delivering to Delivered.
+     * Will also transfer the price of the delivery to the supplier.
+     * If the transfer goes through, the status will be eventually changes to Closed.
+     * @calls Market contract
+     */
+    function deliveredByCourier(uint256 orderId, uint256 companyId) public {
     }
     
 }
