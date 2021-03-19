420c59e111ae9b8b43376b5f3d62d411d005771e
mingweiseow <johnseow.mingwei@gmail.com>
2021-03-19 16:52:09 +0800

    Added login page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,13 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import App from './App.vue'
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import Vue from "vue";
+import "./plugins/bootstrap-vue";
+import App from "./App.vue";
+import Router from "./routes.js";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  router: Router,
+}).$mount("#app");
---
+++ frontend/src/routes.js
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Login from "./pages/Common/Login.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [{ path: "/", name: "login", component: Login }],
+  mode: "history",
+});
