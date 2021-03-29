bc1aa02c392d759d8eae7d5fa1c23ca4eab745cd
Aaron Goh <aarongjy97@gmail.com>
2021-03-29 16:58:41 +0800

    dummy products for suppliers

---
+++ backend/blockchain/assets/products.js
@@ -0,0 +1,55 @@
+const products = {
+  dell: [
+    {
+      name: "Dell XPS 13 2021",
+      quantity: "500",
+      price: "90",
+    },
+    {
+      name: "Dell S2721DGF 27 inch Monitor",
+      quantity: "800",
+      price: "50",
+    },
+    {
+      name: "Dell Inspiron 3881 Desktop",
+      quantity: "300",
+      price: "70",
+    },
+  ],
+  foxconn: [
+    {
+      name: "Foxconn T249 Server Motherboard",
+      quantity: "50",
+      price: "30",
+    },
+    {
+      name: "Foxconn USB Type-C Cable",
+      quantity: "6000",
+      price: "5",
+    },
+    {
+      name: "Foxconn USB Type-C Connector",
+      quantity: "30000",
+      price: "2",
+    },
+  ],
+  tsmc: [
+    {
+      name: "10nm Quad Core Processor",
+      quantity: "5000",
+      price: "25",
+    },
+    {
+      name: "7nm Hexa Core Processor",
+      quantity: "6000",
+      price: "40",
+    },
+    {
+      name: "7nm Quad Core Processor",
+      quantity: "6000",
+      price: "35",
+    },
+  ],
+};
+
+module.exports = products;
